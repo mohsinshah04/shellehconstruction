fbe3c028d0d236024575b1a854ced0f521813949
mohsinshah04 <shahmohsin4882@gmail.com>
2024-10-17 04:45:49 -0500

    fixed bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from "./components/NavBar/navbar";
 import Intro from "./components/Intro/intro";
 import Skills from "./components/Skills/skills";
