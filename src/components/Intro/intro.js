5c5b615244a3e1613989f0a44d04ec102bcfdd4a
mohsinshah04 <shahmohsin4882@gmail.com>
2024-10-17 05:19:27 -0500

    home page

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,20 +1,16 @@
-import React from 'react'
-import './intro.css'
-import bg from '../../assets/MozzeLogo.jpg'
-
+import React from 'react';
+import './intro.css';
+import bg from '../../assets/ShellehMain.png';
 
 const Intro = () => {
-  return (
-    <section id="intro">
-        <div className="introContent">
-            <span className="hello">Hello,</span>
-            <span className="introText">We are <span className="introName">Mozee</span> <br />Motorsports</span>
-            <p className="introPara">We are a world champion hybrid formula <br /> student team based out of the Milwaukee <br /> School of Engineering</p>
-            {/*<button className="btn" onClick={onButtonClick}><img src={btnImg} alt="shop" className='btnImg' />Checkout our Shop</button> */}
-        </div>
-        <img src={bg} alt="Profile" className="bg" />
-    </section>
-  );
+    return (
+        <section id="intro">
+            <img src={bg} alt="Profile" className="bg" />
+            <div className="sloganContainer">
+                <h1 className="slogan">Let's build a better future, starting today</h1>
+            </div>
+        </section>
+    );
 }
 
-export default Intro
+export default Intro;
