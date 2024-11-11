97fd08f41ad8a08ee48b2a5dfd0e779a5fad4209
mohsinshah04 <shahmohsin4882@gmail.com>
2024-11-11 05:07:45 -0600

    our projects page, just need new images in

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -1,16 +1,52 @@
-import React from 'react'
-import './works.css'
-import BeholdWidget from '@behold/react'
+import React from 'react';
+import './works.css';
 
 const Works = () => {
+  const projects = [
+    {
+      id: 1,
+      beforeImage: 'path-to-before-image-1.jpg',
+      afterImage: 'path-to-after-image-1.jpg',
+      description: 'Project 1 description: What was done, the transformation, etc.'
+    },
+    {
+      id: 2,
+      beforeImage: 'path-to-before-image-2.jpg',
+      afterImage: 'path-to-after-image-2.jpg',
+      description: 'Project 2 description: What was done, the transformation, etc.'
+    },
+    {
+      id: 3,
+      beforeImage: 'path-to-before-image-3.jpg',
+      afterImage: 'path-to-after-image-3.jpg',
+      description: 'Project 3 description: What was done, the transformation, etc.'
+    }
+  ];
 
   return (
-    <section id='works'>
+    <section id="works" className="worksSection">
       <h2 className="worksTitle">Our Projects</h2>
       
-      <BeholdWidget feedId='UaeJTMCGan7Zdp7sQULr'/>
+      <div className="projectsContainer">
+        {projects.map(project => (
+          <div key={project.id} className="projectCard">
+            <h3 className="projectTitle">Project {project.id}</h3>
+            <div className="imagesContainer">
+              <div className="imageSlot">
+                <h4>Before</h4>
+                <img src={project.beforeImage} alt={`Before Project ${project.id}`} className="projectImage" />
+              </div>
+              <div className="imageSlot">
+                <h4>After</h4>
+                <img src={project.afterImage} alt={`After Project ${project.id}`} className="projectImage" />
+              </div>
+            </div>
+            <p className="projectDescription">{project.description}</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
-}
+};
 
 export default Works;
